87290da945ca7b5acbb99d72bfca43652a961c1d
Mihovil Santek <msantek@icloud.com>
2020-05-04 17:01:27 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from './components/Header';
+
+import './App.css';
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+      </div>
+    )
+  }
+}
+
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <h2>Expense Tracker</h2>
+  )
+}
+
+export default Header;
