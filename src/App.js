ed03f054da3af21d82d742684404a9eac79bbed5
Mihovil Santek <msantek@icloud.com>
2020-05-04 19:36:27 +0200

    add dinamic properties

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import Header from './components/Header';
+import Balance from './components/Balance';
+import IncomeExpenses from './components/IncomeExpenses';
+import TransactionList from './components/TransactionList';
+import AddTransaction from './components/AddTransaction';
+
+import { GlobalProvider } from './context/GlobalState';
 
 import './App.css';
 
@@ -7,9 +13,17 @@ import './App.css';
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <Header />
-      </div>
+      <GlobalProvider>
+        <div className='main-container'>
+          <Header />
+          <div className='container'>
+            <Balance />
+            <IncomeExpenses />
+            <TransactionList />
+            <AddTransaction />
+          </div>
+        </div>
+      </GlobalProvider>
     )
   }
 }
